56d742c85c03bb13c6bde1fac4332223c5157f71
Maks Trukhan <maxcodejs@gmail.com>
2025-02-22 17:35:38 -0600

    create star in details

--- src/pages/project-detail/projectDetail.jsx
+++ src/pages/project-detail/projectDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './projectDetail'
+import '../main.css'
 import { useNavigate, useParams } from "react-router-dom";
 
 import "./projectDetail.css";
@@ -12,37 +13,51 @@ const ProjectDetail = ({ projects, favoriteProjects, setFavoriteProjects }) => {
   console.log(param);
 
   const chosenProject = projects.find((project) => project.id == projectId);
+  const checkFavorite = favoriteProjects.find(
+    (favorite) => favorite.id == projectId
+  );
   return (
     <div className="detail">
-      {chosenProject !== null && chosenProject !== undefined ? (
+      {chosenProject !== null &&
+      chosenProject !== undefined &&
+      checkFavorite.id !== null ? (
         <>
-          {Object.entries(chosenProject).map(
-            ([key, value]) =>
-             (
-                <>
-                  <div key={key}>
-                    <p className="detail__info">
-                      <span className="detail__title">{key}:</span>
-                      <span className="detail__value">{value}</span>
-                    </p>
-                  </div>
-                </>
-              )
-          )}
-          <div className="detail__btn-flex">
-            <button
-              className=" detail__btn createBtn "
-              onClick={() => navigate("/projects")}
-            >
-              Back
-            </button>
-            <button
-              className=" detail__btn createBtn "
-              onClick={() => navigate(`/projects/${projectId}/edit`)}
-            >
-              Edit
-            </button>
+          <div>
+            {Object.entries(chosenProject).map(([key, value]) => (
+              <>
+                <div key={key}>
+                  <p className="detail__info">
+                    <span className="detail__title">{key}:</span>
+                    <span className="detail__value">{value}</span>
+                  </p>
+                </div>
+              </>
+            ))}
+            <div className="detail__btn-flex">
+              <button
+                className=" detail__btn createBtn "
+                onClick={() => navigate("/projects")}
+              >
+                Back
+              </button>
+              <button
+                className=" detail__btn createBtn "
+                onClick={() => navigate(`/projects/${projectId}/edit`)}
+              >
+                Edit
+              </button>
+            </div>
           </div>
+          <div
+            style={
+              !checkFavorite
+                ? { backgroundColor: "rgb(137, 137, 137)" }
+                : { backgroundColor: "rgb(174, 149, 10)" }
+            }
+            className={`save__project-btn ${
+              checkFavorite ? "savedStar" : "star"
+            }`}
+          />
         </>
       ) : (
         <div className="no__result">
